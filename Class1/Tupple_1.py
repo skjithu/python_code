3c2ff327a079c9a540d97a9f3f3ce16cd2f5a74c
jithesh <skjithu@github.com>
2019-01-26 16:01:25 +0530

    tupple

---
+++ Class1/Tupple_1.py
@@ -0,0 +1,15 @@
+song1 = "Welcome", "Artist", 25
+print(song1)
+
+song2 = ()
+print(song2)
+
+song3 = "ABCD",
+print(song3)
+
+print(len(song3))
+
+#print(song1[1])
+list1 = [1,2,3,4,5]
+song1 = "New Song","Artist 2", 100, list1
+print(song1)
